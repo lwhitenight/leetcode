ddc1fbd9e4127b50110f8c0ca8bfc2fea6b602d3
白夜 <1239140927@qq.com>
2021-01-06 15:36:48 +0800

    增加一个点类

---
+++ Leetcode/src/point.java
@@ -0,0 +1,18 @@
+
+public class point {
+	int x ;
+	int y ;
+	public point(int x,int y) 
+	{
+		this.x =x;
+		this.y=y;
+	}
+	
+	
+	public static void main(String args[]) 
+	{
+		point p = new point(3,4);
+	}
+}
+
+
